f08d775607d0ad75bf4d128698d1e27192de8b68
Louis Larry <louis.larry@gmail.com>
2025-06-04 06:42:53 +0700

    [emacs] add init.el

---
+++ emacs/init.el
@@ -0,0 +1,25 @@
+
+(load-theme 'wheatgrass t)
+
+;; (setq mac-option-modifier 'meta) 
+;; (setq mac-command-modifier 'super)
+
+(setq auto-save-file-name-transforms
+      `((".*" ,(concat user-emacs-directory "auto-save/") t)))
+
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name
+                 (concat user-emacs-directory "backups")))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(lorem-ipsum undo-tree)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
